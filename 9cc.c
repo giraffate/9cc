58ab4043d5ebc0b76cde942d6910e0e9ac7ea8c3
Takayuki Nakata <f.seasons017@gmail.com>
2023-07-27 18:07:01 +0900

    Add a parser and code generator

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//
+// Tokenizer
+//
+
 typedef enum {
     TK_RESERVED,
     TK_NUM,
@@ -105,43 +109,124 @@ Token *tokenize() {
             continue;
         }
 
-        error_at(p, "トークナイズできません");
+        error_at(p, "invalid token");
     }
 
     new_token(TK_EOF, cur, p);
     return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // Node kind
+    Node *lhs;     // Left-hand side
+    Node *rhs;     // Right-hand side
+    int val;       // Used if kind == ND_NUM
+};
+
+Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_num(int val) {
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *num();
+
+// expr = num ("+" num | "-" num)*
+Node *expr() {
+    Node *node = num();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_binary(ND_ADD, node, num());
+        else if (consume('-'))
+            node = new_binary(ND_SUB, node, num());
+        else
+            return node;
+    }
+}
+
+// num
+Node *num() {
+    return new_num(expect_number());
+}
+
+//
+// Code generator
+//
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
+        error("%s: invalid number of arguments", argv[0]);
     }
 
+    // Tokenize and parse.
     user_input = argv[1];
     token = tokenize();
+    Node *node = expr();
 
-    // アセンブリの前半部分を出力
+    // Print out the first half of assembly.
     printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
+    printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして
-    // 最初のmov命令を出力
-    printf("  mov rax, %d\n", expect_number());
+    // Traverse the AST to emit assembly.
+    gen(node);
 
-    // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-    // アセンブリを出力
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-       expect('-');
-       printf("  sub rax, %d\n", expect_number());
-    }
-
+    // A result must be at the top of the stack, so pop it
+    // to RAX to make it a program exit code.
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
